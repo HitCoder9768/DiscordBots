101aae2a5ca17f2ebc253497df6b01dac7eabdb6
HitCoder9768 <hitkid61@gmail.com>
2016-10-26 17:23:39 +0100

    Update D-CleverBot.js

--- D-CleverBot.js
+++ D-CleverBot.js
@@ -1,1 +1,9 @@
 // Cleverbot for Discord made by HitCoder. Feel free to reuse. This script is fairly simplistic so no credit needed.
+
+//Not ready for use yet - unfinished.
+
+var Discordie = require("discordie");
+var cleverbot = require("cleverbot.io");
+
+const botToken = '' // Place your bot's token in here.
+//const 
